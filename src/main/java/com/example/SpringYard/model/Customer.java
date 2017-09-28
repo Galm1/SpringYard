9508573f128974049a9b284c80bf036a1255019d
EliGarcia <garciaelisha93@yahoo.com>
2017-09-28 13:19:54 -0500

    made pojo into jpa entity

--- src/main/java/com/example/SpringYard/model/Customer.java
+++ src/main/java/com/example/SpringYard/model/Customer.java
@@ -1,5 +1,11 @@
 package com.example.SpringYard.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="customer")
 public class Customer {
     Long id;
     String firstName;
@@ -48,5 +54,29 @@ public class Customer {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (id != null ? !id.equals(customer.id) : customer.id != null) return false;
+        if (firstName != null ? !firstName.equals(customer.firstName) : customer.firstName != null) return false;
+        if (lastName != null ? !lastName.equals(customer.lastName) : customer.lastName != null) return false;
+        if (phone != null ? !phone.equals(customer.phone) : customer.phone != null) return false;
+        return email != null ? email.equals(customer.email) : customer.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        result = 31 * result + (phone != null ? phone.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
     }
 }
