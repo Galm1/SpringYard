9be8d4eb7e26134e1e45dffa4c8eb706c68b4a5d
EliGarcia <garciaelisha93@yahoo.com>
2017-09-28 13:23:34 -0500

    made repo into JpaRepo

--- src/main/java/com/example/SpringYard/repository/CustomerRepository.java
+++ src/main/java/com/example/SpringYard/repository/CustomerRepository.java
@@ -1,18 +1,11 @@
 package com.example.SpringYard.repository;
 
 import com.example.SpringYard.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface CustomerRepository {
-    public void addCustomer(Customer customer);
+public interface CustomerRepository extends JpaRepository {
 
-    public void updateCustomer(int id, Customer customer);
-
-    public Customer findCustomerById(Long id);
-
-    public List<Customer> getAllCustomers();
-
-    public void deleteCustomer(int id);
 }
 
