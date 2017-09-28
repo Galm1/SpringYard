924c2ee910fed4a961d87662dddfce22d069e371
EliGarcia <garciaelisha93@yahoo.com>
2017-09-28 13:39:59 -0500

    made controller package and started on making the RestController

---
+++ src/main/java/com/example/SpringYard/controller/CustomerController.java
@@ -0,0 +1,25 @@
+package com.example.SpringYard.controller;
+
+import com.example.SpringYard.service.CustomerService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.security.PublicKey;
+
+@RestController
+public class CustomerController {
+
+    @Autowired
+    CustomerService customerService;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @PostMapping("/api/customer")
+    public String addCustomer(@RequestBody String json) throws IOException {
+
+    }
+}
